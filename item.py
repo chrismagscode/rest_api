3828b2321e6b8173c403441a5959d91eca63cd25
Chris Magcamit <chris.magcamit@gmail.com>
2021-09-07 15:54:35 -0700

    Added DELETE capability

--- item.py
+++ item.py
@@ -2,7 +2,6 @@ from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 import sqlite3
 
-
 class Item(Resource):
     TABLE_NAME = 'items'
 
@@ -12,6 +11,19 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!"
     )
+
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM {table} WHERE name =?".format(table=cls.TABLE_NAME)
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
+
     @classmethod
     def insert(cls, item):
         connection = sqlite3.connect('data.db')
@@ -22,16 +34,18 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-    
-    @classmethod
-    def find_by_name(cls, name):
+        
+    def delete(self, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
-        query = "SELECT * FROM {table} WHERE name=?".format(table=cls.TABLE_NAME)
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
+        query = "DELETE FROM {table} WHERE name=?".format(table=self.TABLE_NAME)
+        cursor.execute(query, (name,))
+
+        connection.commit()
         connection.close()
+
+        return {'message': 'Item deleted'}
 
     def post(self, name):
         if self.find_by_name(name):
@@ -44,10 +58,9 @@ class Item(Resource):
         try:
             Item.insert(item)
         except:
-            return {"message": "An error occurred inserting the item."}
-
+            return {'message': 'An error occurred inserting the item.'}
         return item
-
+    
     def get(self, name):
         item = self.find_by_name(name)
         if item:
