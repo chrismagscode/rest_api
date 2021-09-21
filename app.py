bb1aa2fc8b3a37fed88e82a0e6aa9f1cb80d1eb0
Chris Magcamit <chris.magcamit@gmail.com>
2021-09-21 01:13:33 -0700

    Debugging PostgreSQL environment variable.

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 import os
-import re
-
 
 app = Flask(__name__)
 uri = str(os.getenv("DATABASE_URL"))  # or other relevant config var
@@ -18,7 +16,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
