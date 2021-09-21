0efc5c70b3f758f523f404130279f95295ec2d8e
Chris Magcamit <chris.magcamit@gmail.com>
2021-09-21 00:51:51 -0700

    Adjusting PostgreSQL environment variable

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 import os
+import re
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = str(os.getenv("DATABASE_URL"))  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
