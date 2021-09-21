3b18ad7151358fd93c94f3184caba5fe97c3d777
Chris Magcamit <chris.magcamit@gmail.com>
2021-09-21 01:52:16 -0700

    Fixing PostgreSQL environment variable bug

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('uri', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
